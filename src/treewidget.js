4ddb69aa938011948d6ce07b649b2ccb1456c915
Michael Costa <michaelmcosta@me.com>
2016-08-06 20:07:56 -0400

    u

--- src/treewidget.js
+++ src/treewidget.js
@@ -17,7 +17,64 @@ angular.module('adf.widget.treewidget', ['adf.provider'])
                     controller: 'TreeWidgetConfigCtrl'
 
                 }
-            });
+            }).widget('timelinewidget', {
+              title: 'Time Line',
+              description: 'build a Timeline',
+              templateUrl: '{widgetsPath}/treewidget/src/alt/timeline.html',
+              reload: true,
+              frameless: false,
+              styleClass: 'NOA',
+              controller: 'TimeLineCtrl',
+              edit: {
+                templateUrl: '{widgetsPath}/treewidget/src/alt/timeedit.html',
+                reload: true,
+                controller: 'TimeLineConfigCtrl'
+
+              }
+            }).widget('histogram', {
+              title: '3D Histogram',
+              description: 'build an interactive 3D Histogram Visualization',
+              templateUrl: '{widgetsPath}/treewidget/src/alt/histogram.html',
+              reload: true,
+              frameless: false,
+              styleClass: 'NOA',
+              controller: 'HistogramCtrl',
+              edit: {
+                templateUrl: '{widgetsPath}/treewidget/src/alt/histogramedit.html',
+                reload: true,
+                controller: 'HistogramConfigCtrl'
+
+              }
+            }).widget('revealjs', {
+              title: 'RevealJS Slideshow',
+              description: 'build a presentaion using reveal.js',
+              templateUrl: '{widgetsPath}/treewidget/src/alt/reveal_index.html',
+              reload: true,
+              frameless: false,
+              styleClass: 'NOA',
+              controller: 'RevealCtrl',
+              edit: {
+                templateUrl: '{widgetsPath}/treewidget/src/alt/slide_edit.html',
+                reload: true,
+                controller: 'TreeWidgetConfigCtrl'
+
+              }
+            }).widget('threejs', {
+              title: 'Three.JS 3D World',
+              description: 'build an interactive 3D envirobnent',
+              templateUrl: '{widgetsPath}/treewidget/src/alt/three.html',
+              reload: true,
+              frameless: false,
+              styleClass: 'NOA',
+              controller: 'ThreeWorldCtrl',
+              edit: {
+                templateUrl: '{widgetsPath}/treewidget/src/alt/three_edit.html',
+                reload: true,
+                controller: 'ThreeWorldConfigCtrl'
+
+              }
+            })
+            ;
     })
     .controller('TreeWidgetConfigCtrl', ['$scope', 'config', '$window', '$document', '$compile', '$parse', '$http', 'dashboard', '$sce',
         function($scope, config, $window, $document, $compile, $parse, $http, dashboard, $sce) {
@@ -78,4 +135,77 @@ angular.module('adf.widget.treewidget', ['adf.provider'])
             };
             return config;
         }
-    });
+    }).controller('HistogramCtrl', ["$scope","$stateParams",function($scope,$stateParams){
+        var vm = this;
+        vm.col = $stateParams.pid;
+
+    }]).controller('RevealCtrl', ["$scope", "$stateParams","revealjs","$document","$window", function ($scope, $stateParams, revealjs,$document,$window) {
+      var vm = this;
+      vm.options = {
+        history: true,
+        controls: true,
+        progress: true,
+        slideNumber: false,
+        overview: true,
+        center: true,
+        touch: true,
+        loop: true,
+        rtl: false,
+        shuffle: false,
+        fragments: true,
+        embedded: true,
+        help: true,
+        showNotes: false,
+        autoSlide: 10000,
+        autoSlideStoppable: true,
+        autoSlideMethod: Reveal.navigateNext,
+        mouseWheel: true,
+        hideAddressBar: true,
+        previewLinks: true,
+        transition: 'concave',
+        transitionSpeed: 'slow',
+        backgroundTransition: 'convex',
+        viewDistance: 4,
+        parallaxBackgroundImage: 'https://lexlab.io/llp_core/img/lll3.svg',
+        parallaxBackgroundSize: '1500px 1500px',
+        parallaxBackgroundHorizontal: $window.innerWidth*1.5,
+        parallaxBackgroundVertical:$window.innerHeight*2,
+        dependencies: [
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/markdown/marked.js' },
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/markdown/markdown.js' },
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/notes/notes.js', async: true },
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function () { hljs.initHighlightingOnLoad(); } },
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/zoom-js/zoom.js', async: true },
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/search/search.js', async: true},
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/print-pdf/print-pdf.js', async: true }
+        ]
+      };
+$document.$on('ready', function($evt){
+revealjs().then(function(Reveal){
+
+
+      Reveal.initialize(vm.options);
+  });
+});
+    }]).controller('TimeLineCtrl', ["$scope", "$stateParams","$document","$http","storyjs", function ($scope, $stateParams,$document,$http, storyjs) {
+      var vm = this;
+      $http.get('/treewidget/src/alt/timeline.json').then(function(resp){
+        vm.timelinedata = resp.data;
+      });
+
+      vm.options = {
+        type: 'timeline',
+        width: 800,
+        height: 400,
+        source: vm.timelinedata,
+        embed_id: 'timelinejs',
+        hash_bookmark: true,
+        debug: true,
+        font: 'PTSerif-PTSans'
+      };
+      $document.ready(function(){
+        storyjs().then(function(createStoryJS){
+          createStoryJS(vm.options);
+      });
+      });
+    }]);
