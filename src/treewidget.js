4036bd10f8ebb392b13694c2590943fbaf3ac23c
Michael Costa <michaelmcosta@me.com>
2016-08-07 10:30:58 -0400

    u

--- src/treewidget.js
+++ src/treewidget.js
@@ -25,10 +25,12 @@ angular.module('adf.widget.treewidget', ['adf.provider'])
               frameless: false,
               styleClass: 'NOA',
               controller: 'TimeLineCtrl',
+              controllerAs: 'time',
               edit: {
                 templateUrl: '{widgetsPath}/treewidget/src/alt/timeedit.html',
                 reload: true,
-                controller: 'TimeLineConfigCtrl'
+                controller: 'TimeLineCtrl',
+                controllerAs: 'time'
 
               }
             }).widget('histogram', {
@@ -39,10 +41,11 @@ angular.module('adf.widget.treewidget', ['adf.provider'])
               frameless: false,
               styleClass: 'NOA',
               controller: 'HistogramCtrl',
+              controllerAs: 'histogram',
               edit: {
-                templateUrl: '{widgetsPath}/treewidget/src/alt/histogramedit.html',
+                templateUrl: '{widgetsPath}/treewidget/src/alt/hist_edit.html',
                 reload: true,
-                controller: 'HistogramConfigCtrl'
+                controller: 'HistogramCtrl'
 
               }
             }).widget('revealjs', {
@@ -53,10 +56,12 @@ angular.module('adf.widget.treewidget', ['adf.provider'])
               frameless: false,
               styleClass: 'NOA',
               controller: 'RevealCtrl',
+              controllerAs: 'reveal',
               edit: {
                 templateUrl: '{widgetsPath}/treewidget/src/alt/slide_edit.html',
                 reload: true,
-                controller: 'TreeWidgetConfigCtrl'
+                controller: 'RevealCtrl',
+                controllerAs: 'reveal'
 
               }
             }).widget('threejs', {
@@ -135,12 +140,72 @@ angular.module('adf.widget.treewidget', ['adf.provider'])
             };
             return config;
         }
-    }).controller('HistogramCtrl', ["$scope","$stateParams",function($scope,$stateParams){
+    }).controller('HistogramCtrl', ['$scope','$stateParams','$templateCache','Collection','config', function($scope,$stateParams,$templateCache, Collection, config){
         var vm = this;
-        vm.col = $stateParams.pid;
+        config = config || $scope.$parent.config;
+        vm.col = config.id;
+        Collection(vm.col).$loaded().then(function(collection){
 
-    }]).controller('RevealCtrl', ["$scope", "$stateParams","revealjs","$document","$window", function ($scope, $stateParams, revealjs,$document,$window) {
+
+        var tpl = $templateCache.get('bartpl');
+        var $ = window.$;
+        var Modernizr = window.Modernizr;
+
+        $('.histogram-3d td').each(function () {
+          var val = this.innerHTML;
+          $(this)
+            .html(tpl)
+            .css('font-size', val + 'px')
+            ;
+        });
+          var dragStart = {}, dragging = false, curpos = {x:100, y: -75};
+          var touch = Modernizr.touch, $vp = $('.viewport:first');
+          $vp.on(touch?'touchstart': 'mousedown', function(e) {
+
+    var evt = touch?e.originalEvent.touches[0]: e;
+          dragStart = {
+            x: evt.screenX + curpos.x,
+          y: evt.screenY + curpos.y
+          };
+
+          dragging = true;
+          $('body').addClass('noselect');
+          });
+
+          $(document).on(touch?'touchend': 'mouseup', function() {
+            dragging = false;
+          $('body').removeClass('noselect');
+          });
+
+          $(document).on(touch?'touchmove': 'mousemove', function(e) {
+
+    if (!dragging) return;
+
+          e.preventDefault();
+
+          var evt = touch?e.originalEvent.touches[0]: e
+          , x = dragStart.x - evt.screenX
+          , y = dragStart.y - evt.screenY
+          , amp = 0.2
+          ;
+
+          curpos.x = x;
+          curpos.y = y;
+
+          $vp.find('.world').css(
+          Modernizr.prefixed('transform'),
+          ['rotateX(', y*amp, 'deg) rotateY(', -x*amp, 'deg)'].join('')
+          );
+
+          });
+        });
+
+    }]).controller('RevealCtrl', ['$scope', '$stateParams','revealjs','$document','$window','$css','toastr', function ($scope, $stateParams, revealjs,$document,$window,$css, toastr) {
       var vm = this;
+      vm.csssources = ['https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css','/lexlab-starter/node_modules/reveal.js/css/reveal.css','/lexlab-starter/node_modules/reveal.js/css/theme/black.css','/lexlab-starter/node_modules/reveal.js/lib/css/zenburn.css'];
+      $css.add(vm.csssources, function(msg){
+        toastr.info(msg);
+      });
       vm.options = {
         history: true,
         controls: true,
@@ -175,37 +240,121 @@ angular.module('adf.widget.treewidget', ['adf.provider'])
           { src: '/lexlab-starter/node_modules/reveal.js/plugin/markdown/markdown.js' },
           { src: '/lexlab-starter/node_modules/reveal.js/plugin/notes/notes.js', async: true },
           { src: '/lexlab-starter/node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function () { hljs.initHighlightingOnLoad(); } },
-          { src: '/lexlab-starter/node_modules/reveal.js/plugin/zoom-js/zoom.js', async: true },
-          { src: '/lexlab-starter/node_modules/reveal.js/plugin/search/search.js', async: true},
-          { src: '/lexlab-starter/node_modules/reveal.js/plugin/print-pdf/print-pdf.js', async: true }
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/zoom-js/zoom.js', async: true }
+          /*{ src: '/lexlab-starter/node_modules/reveal.js/plugin/search/search.js', async: true},
+          { src: '/lexlab-starter/node_modules/reveal.js/plugin/print-pdf/print-pdf.js', async: true }*/
         ]
       };
-$document.$on('ready', function($evt){
-revealjs().then(function(Reveal){
+
+revealjs.Reveal().then(function(){
 
 
-      Reveal.initialize(vm.options);
+      window.Reveal.initialize(vm.options);
   });
-});
-    }]).controller('TimeLineCtrl', ["$scope", "$stateParams","$document","$http","storyjs", function ($scope, $stateParams,$document,$http, storyjs) {
+    }]).controller('TimeLineCtrl', ['$scope', '$stateParams','$document','$http','storyjs','config','Collection','toastr','$filter', function ($scope, $stateParams,$document,$http, storyjs, config, Collection,toastr, $filter) {
       var vm = this;
-      $http.get('/treewidget/src/alt/timeline.json').then(function(resp){
-        vm.timelinedata = resp.data;
-      });
 
-      vm.options = {
-        type: 'timeline',
-        width: 800,
-        height: 400,
-        source: vm.timelinedata,
-        embed_id: 'timelinejs',
-        hash_bookmark: true,
-        debug: true,
-        font: 'PTSerif-PTSans'
+
+
+      config = config || $scope.$parent.config;
+      vm.data = {
+        'timeline': {
+          'headline': 'Prosecution History Digest',
+          'type': 'default',
+          'text': 'US 8,382,656',
+          'asset': {
+            'media': '/patents/8382656/preview',
+            'credit': 'Lion Legal Products',
+            'caption': 'Master the Jungle of Legal Information'
+          },
+          'date': [{
+            'startDate': '2000-01-01',
+            'endDate': '2000-01-01',
+            'headline': 'Initial Entry',
+            'text': 'This is a poodle',
+            'asset': {
+              'media': '/llp_core/img/lll3.svg',
+              'credit': 'LLP',
+              'caption': '',
+              'thumbnail': '/files/public/uspto/patents/8382656.png',
+              'type': 'image/png',
+              'tag': 'test'
+            }
+          }]
+        }
       };
-      $document.ready(function(){
-        storyjs().then(function(createStoryJS){
-          createStoryJS(vm.options);
-      });
-      });
+      Collection(config.id).$loaded().then(function(collection){
+
+        // vm.data = {
+        //   'timeline': {
+        //     'headline': 'Prosecution History Digest',
+        //     'type': 'default',
+        //     'text': collection.id + ' - ' +  collection.title,
+        //     'asset': {
+        //       'media': '/patents/'+collection.id+'/preview',
+        //       'credit': 'Lion Legal Products',
+        //       'caption': 'Master the Jungle of Legal Information'
+        //     },
+        //     'date': [{
+        //       'startDate': collection.date,
+        //       'endDate': collection.date,
+        //       'headline': collection.title,
+        //       'text': collection.description,
+        //       'asset': {
+        //         'media': collection.media,
+        //         'credit': collection.styleClass,
+        //         'caption': '',
+        //         'thumbnail': collection.thumbnail || collection.media,
+        //         'type': collection.styleClass,
+        //         'tag': collection.rid
+        //       }
+        //     }]
+        //   }
+        // };
+
+
+            angular.forEach(collection.roarlist, function(rid, key){
+              Collection(rid).$loaded().then(function(rvent){
+                var thisobj = {
+                  'startDate': rvent.date,
+                  'endDate': rvent.date,
+                  'headline': rvent.title,
+                  'text': rvent.description,
+                  'asset': {
+                    'media': rvent.media,
+                    'credit': rvent.styleClass,
+                    'caption': '',
+                    'thumbnail': rvent.thumbnail || rvent.media,
+                    'type': rvent.styleClass,
+                    'tag': rvent.rid
+                }};
+                toastr.info($filter('date')(rvent.date),rvent.title);
+                  vm.data.timeline.date.push(thisobj);
+
+                  storyjs.createStoryJS().then(function (createStoryJS) {
+                  vm.options = {
+                    type: 'timeline',
+                    width: 800,
+                    height: 600,
+                    source: vm.data,
+                    embed_id: 'timelinejs',
+                    hash_bookmark: false,
+                    debug: true,
+                    font: 'PTSerif-PTSans'
+                  }; createStoryJS(vm.options); });
+              });
+
+
+
+            });
+
+
+    });
+
+
+
+
+
+
+
     }]);
