d730c06a85b20b7a40621f3ee36af0354c236b01
Michael Costa <micosuave@MICOFF.local>
2016-08-11 15:10:24 -0400

    u

--- dist/adf-widget-treewidget.js
+++ dist/adf-widget-treewidget.js
@@ -520,7 +520,7 @@ function buildslides (slidearray){
       };
       vm.save = function(){
         var ref = Collection(config.id).$ref();
-        ref.child('timeline').update(angular.toJson(vm.data));
+        ref.update({'timeline': angular.toJson(vm.data)});
         // Collection(config.id).$loaded().then(function(vmodel){
         //   vmodel.timeline = vm.data;
         //   vmodel.$save();
