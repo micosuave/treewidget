c58fcc4e2242673a02e3cba48ceedbfe7e510aae
Michael Costa <michaelmcosta@me.com>
2016-08-21 11:08:47 -0400

    u

--- dist/adf-widget-treewidget.js
+++ dist/adf-widget-treewidget.js
@@ -375,7 +375,7 @@ vm.slides.push(roarevent);
         else{
           var tol= slide.slide || '<!--CUTSLIDEHEAD-->';
           var colly = slide.content || '<!--CUTSLIDEHEAD-->';
-         thishtml = thishtml + '  <section>  ' +colly.slice(colly.indexOf(/(?:\<body.*?\>)./i), colly.indexOf('<script')) + '  </section>  ';
+         thishtml = thishtml + '  <section>  ' +colly.slice(colly.indexOf('<!--CUTSLIDEHEAD-->'), colly.indexOf('<script')) + '  </section>  ';
       //          thishtml = thishtml + '  <section data-background-image="url('+slide.media+')">  ' + '<h2>' +slide.title +'</h2><hr/>'+ slide.slide.slice(slide.slide.indexOf('<body'),slide.slide.indexOf('<script')) + '  </section>  ';
 
       //thishtml = thishtml + tol;
